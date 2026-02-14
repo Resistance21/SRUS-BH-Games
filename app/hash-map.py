df1882148ee9df36b8c4e723773d9f77044b3765
Brendan Hales <20167808@tafe.wa.edu.au>
2026-02-15 00:36:48 +0800

    added the get_index, start the setitem and added the __hash__ to Player

--- app/hash-map.py
+++ app/hash-map.py
@@ -7,19 +7,46 @@ class PlayerHashMap:
     def __init__(self):
         self.hashmap = [PlayerList() for _ in range(self.SIZE)]
 
+    def __len__(self):
+        return len(self.hashmap)
+    
+    def __getitem__(self, index: int):
+        return self.hashmap[index]
+    
+    def __setitem__(self, index: int, player: PlayerNode):
+        self.hashmap[index].insert_node(player)
+
+    def get_index(self, key: str | Player) -> int:
+        if isinstance(key, Player):
+            return hash(key) % self.SIZE
+        else:
+            return Player.hash(key) % self.SIZE
+
 player_map = PlayerHashMap()
 
 for playerlist in player_map.hashmap:
     print (hex(id(playerlist)))
 
-player_map.hashmap[0].insert_node(PlayerNode(Player('3', 'test head')))
-# player_map.hashmap[0].insert_node(PlayerNode(Player('10', 'test mid')))
-# player_map.hashmap[0].insert_node(PlayerNode(Player('15', 'test last')))
-player_map.hashmap[1].insert_node(PlayerNode(Player('1', 'test2')))
-player_map.hashmap[2].insert_node(PlayerNode(Player('5', 'test3')))
-print (player_map.hashmap[0])
-print (player_map.hashmap[1])
-print (player_map.hashmap[2])
+player_1 = Player('4','test player 1')
+player_2 = Player('7','test player 2')
 
-player_map.hashmap[0].delete_player_at_key('3')
-print (player_map.hashmap[0])
+print(player_map.get_index(Player('0','test 5')))
+print(player_map.get_index(Player('0','test 5')))
+print(player_map.get_index(Player('0','test 5')))
+print(player_map.get_index(Player('0','test 5')))
+print(player_map.get_index(player_1))
+print(player_map.get_index(player_1))
+print(player_map.get_index(player_2))
+print(player_map.get_index(player_1))
+
+# player_map.hashmap[0].insert_node(PlayerNode(Player('3', 'test head')))
+# # player_map.hashmap[0].insert_node(PlayerNode(Player('10', 'test mid')))
+# # player_map.hashmap[0].insert_node(PlayerNode(Player('15', 'test last')))
+# player_map.hashmap[1].insert_node(PlayerNode(Player('1', 'test2')))
+# player_map.hashmap[2].insert_node(PlayerNode(Player('5', 'test3')))
+# print (player_map.hashmap[0])
+# print (player_map.hashmap[1])
+# print (player_map.hashmap[2])
+
+# player_map.hashmap[0].delete_player_at_key('3')
+# print (player_map.hashmap[0])
--- app/player.py
+++ app/player.py
@@ -10,6 +10,14 @@ class Player:
     def name(self) -> str:
         return self._name
     
+    def __eq__(self, other):
+        if isinstance(other, Player):
+            return self.uid == other.uid
+        return self.uid == other
+    
+    def __hash__(self):
+        return hash(self.uid) 
+    
     def __str__(self) -> str:
         return f"Player ID: {self._uid}, with the name of: {self._name}"
     
