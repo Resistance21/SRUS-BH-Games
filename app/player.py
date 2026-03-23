1eb8124c931fa6f44c0676bd403dc593a256f745
Brendan Hales <20167808@tafe.wa.edu.au>
2026-03-23 11:16:46 +0800

    completion of task 3.1

--- app/player.py
+++ app/player.py
@@ -4,9 +4,10 @@ Player class that sets up user id and name
 
 
 class Player:
-    def __init__(self, uid: str, name: str):
+    def __init__(self, uid: str, name: str, score: int = 0):
         self._uid = uid
         self._name = name
+        self._score = score
 
     # gets and returns the player uid
     @property
@@ -18,6 +19,16 @@ class Player:
     def name(self) -> str:
         return self._name
 
+    @property
+    def score(self) -> int:
+        return self._score
+
+    @score.setter
+    def score(self, score):
+        if score < 0:
+            raise ValueError("Score needs to be a positive value")
+        self._score = score
+
     @classmethod
     def string_or_player_hash(cls, key: str) -> int:
         return hash(int(key))
@@ -28,3 +39,6 @@ class Player:
     # string representation of the player
     def __str__(self) -> str:
         return f"Player ID: {self._uid}, with the name of: {self._name}"
+
+    def __repr__(self):
+        return f"{self.__class__.name}(name='{self.name}, uid={self.uid}, score={self.score}"
